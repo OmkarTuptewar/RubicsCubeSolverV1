74d5b6b5f82aa27826f0177c465a59a832ba67b7
OmkarTuptewar <omkarsanjay2004@gmail.com>
2025-02-15 17:03:05 +0530

    Added GenericRUbiccube Model

---
+++ GenericRubicsCube.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Omkar tuptewar on 15-02-2025.
+//
+
+#include "GenericRubicsCube.h"
---
+++ GenericRubicsCube.h
@@ -0,0 +1,16 @@
+//
+// Created by Omkar tuptewar on 15-02-2025.
+//
+
+#ifndef GENERICRUBICSCUBE_H
+#define GENERICRUBICSCUBE_H
+
+
+
+class GenericRubicsCube {
+
+};
+
+
+
+#endif //GENERICRUBICSCUBE_H
